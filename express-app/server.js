a3493c4d9941c6582c882bfb2b139459e694790d
Varun Sudrik <varunsudrik99@gmail.com>
2024-05-02 08:44:18 +0530

    commit

---
+++ express-app/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! v1");
+});
+app.get("/health", (req, res) => {
+  res.send("Health check");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
