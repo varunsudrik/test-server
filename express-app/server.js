c213de49b9dcff1b0f7e7dbd0237d731d54c14c9
Varun Sudrik <varunsudrik99@gmail.com>
2024-05-02 20:53:08 +0530

    Update server.js

--- express-app/server.js
+++ express-app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! v2");
+  res.send("Hello, World! v3");
 });
 app.get("/health", (req, res) => {
   res.send("Health check");
